5bd89102120f13c3fab0cec00ca4fa62d6c86a2a
alex <53851759+alxxjohn@users.noreply.github.com>
2023-04-18 13:43:35 -0400

    Save

--- main.go
+++ main.go
@@ -9,21 +9,23 @@ var OrderNum int = 1234
 
 func main() {
 
-	option2:= "Ford Pinto"
-	option1 := "Ford Focus"
-	grouped := option1 +  " "  + option2
+	option2 := "2018 Bergamont City"
+	option1 := "2020 Gazelle Medeo"
+	grouped := option1 + " " + option2
 	splits := strings.Split(grouped, " ")
 	fmt.Println(splits)
 
+	var option2new string
+	var option1new string
+
 	for i := 0; i < len(option2); i++ {
+		if option1[i] != option2[i] {
 
-		fmt.Printf("%c\n", option1[i])
-
+			option2new += string(option2[i:])
+			option1new += string(option1[i:])
+			break
+		} 
 	}
-
-
-
-
+	fmt.Println(option2new)
+	fmt.Println(option1new)
 }
-
-
