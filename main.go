d3f1cde2c6c23d5b3573ac2ace207debb25f33f9
alex <53851759+alxxjohn@users.noreply.github.com>
2023-04-18 12:49:19 -0400

    Save

--- main.go
+++ main.go
@@ -2,19 +2,27 @@ package main
 
 import (
 	"fmt"
-	_ "time"
+	"strings"
 )
 
 var OrderNum int = 1234
 
 func main() {
 
-	carCount := 21
-	group := 10
-	total := carCount / group
-	remainder := carCount % 10
-	fmt.Println(total)
-	fmt.Println(remainder)
+	option2:= "Ford Pinto"
+	option1 := "Ford Focus"
+	grouped := option1 +  " "  + option2
+	splits := strings.Split(grouped, " ")
+	fmt.Println(splits)
+
+	for i := 0; i < len(option2); i++ {
+
+		fmt.Printf("%c\n", option1[i])
+
+	}
+
+
+
 
 }
 
